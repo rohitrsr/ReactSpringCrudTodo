cd089234e61f62e843fd2ff91efbf4320a8868ea
rohitrathour <rrathour.rohit23@gmail.com>
2021-11-01 10:58:14 +0530

    created react and spring app folders

---
+++ springtodo/src/main/java/com/spring/react/springtodo/SpringtodoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.react.springtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtodoApplication.class, args);
+	}
+
+}
